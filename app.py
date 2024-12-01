151f44ec13530f5990ba311389f0c600af5788d4
tmkttmkt <24rd123@ms.dendai.ac.jp>
2024-12-01 22:42:10 +0900

    no message

--- app.py
+++ app.py
@@ -14,20 +14,32 @@ bot = discord.Bot()
 async def run_discord_bot():
     """非同期でDiscordボットを実行"""
     await bot.start(os.getenv("DISCORD_TOKEN"))
-
+async def stop_discord_bot():
+    await bot.close() 
 # Discordボットのイベント
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
 
+@bot.command()
+async def test(ctx):
+    await ctx.respond("かかって来いよ")
 ################################################################################################################################################################
 
 
 # FastAPIのルート
 @app.get("/")
 async def read_root():
-    return {"message": "Discord bot is running"}
+    return "hello route"
+@app.get("/start")
+async def read_root():
+    asyncio.create_task(run_discord_bot())
+    return "セットアップ"
+@app.get("/stop")
+async def read_root():
+    asyncio.create_task(stop_discord_bot())
+    return "ストップダウン"
 @app.on_event("startup")
 async def startup_event():
     asyncio.create_task(run_discord_bot())
