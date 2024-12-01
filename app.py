78b77943941174a0418f892e58116319c1e9617d
tmkttmkt <24rd123@ms.dendai.ac.jp>
2024-12-01 14:00:52 +0900

    kk

--- app.py
+++ app.py
@@ -1,37 +1,3 @@
-from flask import Flask
-import discord
-import os
-app = Flask(__name__)
-
-
-# Botの設定
-bot = discord.Bot()
-
-@bot.event
-async def on_ready():
-    print(f"Bot logged in as {bot.user}")
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Hello, world!")
-
-
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
-@app.route('/start')
-def start():
-  bot.run(os.getenv("DISCORD_TOKEN"))
-  return 'start'
-@app.route('/stop')
-def stop():
-  bot.close()
-  return 'stop'
-
-
-if __name__ == "__main__":
-   app.run(debug=True, use_reloader=False)
-
 from fastapi import FastAPI
 import os
 import discord
@@ -66,6 +32,5 @@ async def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-
     # FastAPIアプリケーションを非同期で起動
     uvicorn.run(app, host="0.0.0.0", port=8000)
